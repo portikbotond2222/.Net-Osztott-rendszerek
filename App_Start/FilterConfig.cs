864444cb54a79287e03347420440e5adc20f4615
Portik Botond <portik.botond@innoweb.ro>
2019-03-25 08:38:01 +0200

    inti commit

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Osztott
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Scripts/Users.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    $('#saveBtn').click(function () {
+        var fname = $('#fname').val();
+        var lname = $('#lname').val();
+        var email = $('#email').val();
+        var psw = $('#psw').val();
+        var psw2 = $('#psw2').val();
+        if (fname && lname && email && psw && psw2 && (psw === psw2)) {
+            $.ajax({
+                url:  '/site/watchdelay',
+                method: 'post',
+                async: false,
+                data: {
+                    district: district,
+                    street: street,
+                    houseNumber: houseNumber,
+                },
+                success: function (response) {
+                    obj = JSON.parse(response);
+                    if (obj > 0) {
+                        delaymessage(obj);
+                    }
+                    else {
+                        $('#delaymessage').addClass('hide');
+                    }
+                }
+            })
+        }
+        console.log(fname);
+    })
+})
